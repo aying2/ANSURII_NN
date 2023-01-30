9b6e512718b5b0434a874fc9ea0937e9fe269aed
andrew <andrew@localhost.localdomain>
2023-01-30 10:47:41 -0500

    reading csv

---
+++ main.py
@@ -0,0 +1,7 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+male_frame = pd.read_csv('ANSUR_II_MALE_Public.csv')
+male_frame.iloc
